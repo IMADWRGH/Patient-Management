faef0d65d31e48c29a6e31cc0093117185cfa051
Imad Asserne <imad.asserne@gmail.com>
2025-03-14 00:30:25 +0000

    first commit

---
+++ patient-service/src/main/java/com/imadwrgh/patientservice/model/Patient.java
@@ -0,0 +1,39 @@
+package com.imadwrgh.patientservice.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @NotNull
+    private String name;
+
+    @Email
+    @NotNull
+    @Column(unique = true)
+    private String email;
+
+    @NotNull
+    private String address;
+
+    @NotNull
+    private LocalDate birthDate;
+
+    @NotNull
+    private LocalDate registerDate;
+}
