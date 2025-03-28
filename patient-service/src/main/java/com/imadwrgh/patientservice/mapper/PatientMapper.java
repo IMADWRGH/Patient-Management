2fb3227b628c38f3c2255770d8880d244461218d
Imad Asserne <imad.asserne@gmail.com>
2025-03-28 01:04:23 +0000

    add getter & setter & test api

--- patient-service/src/main/java/com/imadwrgh/patientservice/mapper/PatientMapper.java
+++ patient-service/src/main/java/com/imadwrgh/patientservice/mapper/PatientMapper.java
@@ -5,14 +5,24 @@ import com.imadwrgh.patientservice.model.Patient;
 
 public class PatientMapper {
 
-    public static PatientResponseDTO toDTO(Patient patient){
-        PatientResponseDTO patientResponseDTO= new PatientResponseDTO();
+    public static PatientResponseDTO toDTO(Patient patient) {
+        PatientResponseDTO patientResponseDTO = new PatientResponseDTO();
         patientResponseDTO.setId(patient.getId().toString());
         patientResponseDTO.setName(patient.getName());
-        patientResponseDTO.setEmail(patientResponseDTO.getEmail());
-        patientResponseDTO.setAddress(patientResponseDTO.getAddress());
-        patientResponseDTO.setRegisterDate(patientResponseDTO.getRegisterDate());
-        patientResponseDTO.setBirthDate(patientResponseDTO.getBirthDate());
+        patientResponseDTO.setEmail(patient.getEmail());
+        patientResponseDTO.setAddress(patient.getAddress());
+        patientResponseDTO.setRegisterDate(patient.getRegisterDate().toString());
+        patientResponseDTO.setBirthDate(patient.getBirthDate().toString());
         return patientResponseDTO;
     }
+
+//    public static Patient toEntity(PatientResponseDTO dto){
+//        Patient patient=new Patient();
+//
+//        patient.setName(dto.getName());
+//        patient.setEmail(dto.getEmail());
+//        patient.setAddress(dto.getAddress());
+//
+//        return patient;
+//    }
 }
