d9424fde1ffee56c7c6f9186e9fcaca270d75c98
Imad Asserne <imad.asserne@gmail.com>
2025-04-19 23:09:17 +0000

    add toEntity method

--- patient-service/src/main/java/com/imadwrgh/patientservice/mapper/PatientMapper.java
+++ patient-service/src/main/java/com/imadwrgh/patientservice/mapper/PatientMapper.java
@@ -1,7 +1,10 @@
 package com.imadwrgh.patientservice.mapper;
 
+import com.imadwrgh.patientservice.dto.PatientRequestDTO;
 import com.imadwrgh.patientservice.dto.PatientResponseDTO;
 import com.imadwrgh.patientservice.model.Patient;
+
+import java.time.LocalDate;
 
 public class PatientMapper {
 
@@ -16,13 +19,13 @@ public class PatientMapper {
         return patientResponseDTO;
     }
 
-//    public static Patient toEntity(PatientResponseDTO dto){
-//        Patient patient=new Patient();
-//
-//        patient.setName(dto.getName());
-//        patient.setEmail(dto.getEmail());
-//        patient.setAddress(dto.getAddress());
-//
-//        return patient;
-//    }
+    public static Patient toEntity(PatientRequestDTO patientRequestDTO) {
+        Patient patient = new Patient();
+        patient.setName(patientRequestDTO.getName());
+        patient.setAddress(patientRequestDTO.getAddress());
+        patient.setEmail(patientRequestDTO.getEmail());
+        patient.setBirthDate(LocalDate.parse(patientRequestDTO.getDateOfBirth()));
+        patient.setRegisterDate(LocalDate.parse(patientRequestDTO.getRegisteredDate()));
+        return patient;
+    }
 }
